148b27bfd67c7430d8b29bb5a5a416973106ff45
Cole <cadumas01@gmail.com>
2021-09-03 11:42:45 -0400

    homework 00

---
+++ homework00/nth_prime.py
@@ -0,0 +1,35 @@
+import math
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+# key = n, value = nth_prime
+primes = {1: 2}
+
+
+def nth_prime(n):
+    if n in primes:
+        return primes[n]
+    i = 2
+    prime_count = 1
+    while prime_count < n:
+        i += 1
+        if is_prime(i):
+            prime_count += 1
+            primes[prime_count] = i
+
+    return i
+
+
+if __name__ == "__main__":
+    while True:
+        n = int(input('> '))
+
+        print(nth_prime(n))
---
+++ homework00/prime_factorization.py
@@ -0,0 +1,31 @@
+import math
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_factorization(n):
+    factors = []
+    while not is_prime(n):
+        i = 2
+        while i < int(math.sqrt(n) + 1):
+            if n % i == 0:
+                factors.append(i)
+                n = n / i
+                i = 2
+            else:
+                i += 1
+    factors.append(int(n))
+    return factors
+
+
+if __name__ == "__main__":
+    while True:
+        n = int(input('> '))
+        print(prime_factorization(n))
